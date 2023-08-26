4bc6e1cc092fdfc000a1894c873504e47d8aa5f0
anzhiyu <73864136+anzhiyu-c@users.noreply.github.com>
2023-08-26 10:38:19 +0800

    回退

--- api/proxy.js
+++ api/proxy.js
@@ -2,7 +2,7 @@
 const allowedPackages = require("../allowed-packages.json");
 
 module.exports = async (req, res) => {
-  const { packageName, version, path, needEnd } = req.query;
+  const { packageName, version, path } = req.query;
 
   const packageConfig = allowedPackages.find(pkg => pkg.name === packageName);
   console.log(packageName, version, path);
@@ -26,8 +26,7 @@ module.exports = async (req, res) => {
 
   const versionSegment = version ? `@${version}` : "";
   const pathSegment = path ? `/${path}` : "/";
-  const needEndSegment = needEnd ? "/" : "";
-  const url = `https://cdn.jsdelivr.net/npm/${packageName}${versionSegment}${pathSegment}${needEndSegment}`;
+  const url = `https://cdn.jsdelivr.net/npm/${packageName}${versionSegment}${pathSegment}`;
   console.log("url:", url);
   const fetchModule = await import("node-fetch");
   const fetch = fetchModule.default;
